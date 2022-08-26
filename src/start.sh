b34170c6f0bd94be6457628317056a0e8b16a778
Adam Holliday <dochollidayxx@gmail.com>
2022-08-26 15:35:23 -0400

    Giving up on size issues...

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,2 @@
 # !/bin/bash
-echo "Installing Satisfactory in location: $GAMEDIR"
-$STEAMCMDDIR/steamcmd.sh +force_install_dir $GAMEDIR +login anonymous +app_update 1690800 validate +quit
 $GAMEDIR/FactoryServer.sh
