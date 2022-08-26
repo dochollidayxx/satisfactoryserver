a76b1dec283a91d21d4f951813fe24f36f720761
Adam Holliday <dochollidayxx@gmail.com>
2022-08-26 00:03:05 -0400

    initial commit

---
+++ run.cmd
@@ -0,0 +1,1 @@
+docker run -p 127.0.0.1:15777:15777/udp -p 127.0.0.1:15000:15000/udp -p 127.0.0.1:7777:7777/udp -d -v svolume:/home/steam/SatisfactoryDedicatedServer -v sconfigvolume:/home/steam/.config satisfactoryserver
